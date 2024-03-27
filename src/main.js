cdfee0ae4e576017a923dcd74a5624fc90234599
Krish Kalvani <k4spidey@gmail.com>
2024-03-27 19:11:21 +0400

    added the PWA app

--- src/main.js
+++ src/main.js
@@ -2,3 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/serviceWorker.js').then(registration => {
+        console.log('SWBackend registered: ', registration);
+      }).catch(registrationError => {
+        console.log('SWBackend registration failed: ', registrationError);
+      });
+    });
+}
