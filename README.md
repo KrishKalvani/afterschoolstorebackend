301ff0909a2241c859a2b9bb51201411304c9e94
Krish Kalvani <k4spidey@gmail.com>
2024-04-02 09:33:45 +0400

    readme file updated with link

--- README.md
+++ README.md
@@ -22,3 +22,6 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+# link to github repository
+https://github.com/KrishKalvani/afterschoolstorebackend
--- public/service-worker.js
+++ public/service-worker.js
@@ -22,10 +22,10 @@ var cacheFiles = [
 
 //actually adding to the cache here
 self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Install');
+    // console.log('[Service Worker] Install');
     e.waitUntil(
         caches.open(cacheName).then((cache) =>{
-            console.log('[Service Worker] Caching all the files');
+            // console.log('[Service Worker] Caching all the files');
             return cache.addAll(cacheFiles);
         })
     );
